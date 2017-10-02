d69b59eb5d237d4d4e0698c5625932b77d0a0eca
Ubuntu <ubuntu@ip-172-31-28-13.us-east-2.compute.internal>
2017-10-02 01:17:13 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # initialize_user
 
-TODO: Enter the cookbook description here.
+This is a Chef cookbook to create a new user on a Linux host, set up a home directory and drop a fake SSH key into its .ssh directory
+
+## Additional details
+
+None yet
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,23 @@
 # Cookbook:: initialize_user
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, The Authors, All Rights Reserved
+user 'serviceuser' do
+  manage_home true
+  home '/home/serviceuser'
+  shell '/bin/bash'
+  password 'servicepassword'
+end
+
+directory '/home/serviceuser/.ssh' do
+  recursive true
+  action :create
+end
+
+cookbook_file '/home/serviceuser/.ssh/id_rsa' do
+  source 'sampleFile'
+  mode '0600'
+  owner 'serviceuser'
+  group 'serviceuser'
+  action :create
+end
